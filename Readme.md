8d384702c7f81956abbaa287ae9a86286b6d2841
ika-gyosen(sakai) <s.sakai4223@gmail.com>
2021-12-29 15:30:37 +0900

    wip

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# webpack-typescript-express
+
+webpack+typescriptで以下の例が動く最低限の状態？
+
+[github:express examples/cookie-sessions](https://github.com/expressjs/express/blob/master/examples/cookie-sessions/index.js)
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import express from "express";
-// import cookieSession from "cookie-session";
+import cookieSession from "cookie-session";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-// app.use(cookieSession({ secret: "test secret" }));
-// app.use(count);
+app.use(cookieSession({ secret: "test secret" }));
+app.use(count);
 
-// function count(req: Express.Request, res: Express.Response) {
-//   if (!req.session) return;
-//   req.session["count"] = "a";
-// }
+function count(req: Request, res: Response) {
+  console.log(req.session);
+  if (!req.session) return;
+  req.session.count = (req.session.count || 0) + 1;
+  res.send("viewed " + req.session.count + " times\n");
+}
 
 app.listen(4000);
 console.log("express started 🕙 http://localhost:4000");
